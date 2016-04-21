4ad70e14fef8e64003174c7ae3c3d7c93334f651
Markos Rendell <markosrendell@gmail.com>
2016-04-21 15:26:11 +0100

    Fixing newline
    Signed-off-by: Markos Rendell <markosrendell@gmail.com>

--- jenkins/jobs/dsl/flyway_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/flyway_pipeline_jobs.groovy
@@ -193,9 +193,7 @@ packageSql.with{
           buildNumber('${B}')
       }
     }
-    shell('''set -x
-            |zip -r '''.stripMargin() +
-            |referenceAppGitRepo + ${B}.zip'''.stripMargin())
+    shell('''set -x zip -r ''' + referenceAppGitRepo + ${B}.zip''')
   }
   publishers{
     archiveArtifacts("**/*zip")
